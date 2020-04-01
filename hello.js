32f783559f896edcdba999e3e2df55093170f995
Ohhong <48611456+Ohhong@users.noreply.github.com>
2020-04-01 16:39:21 +0900

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,54 +1,52 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const querystring = require('querystring');
+app.use(express.json());
 require('date-utils');
+port = 8000;
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-app.use(bodyParser.json());
-
-app.get('/get', function (req, res) {
-  var results = "{";
-  var get_query = querystring.parse(req.url);
-  var flag = 0;
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  var date = new Date();
-  var time = date.toFormat('YYYY-MM-DD HH:MI:SS');
-  
-  for(var i in get_query) {
-    if(flag == 0) {
-      if(i === '/get' || i === '/get?') break;
-      results = results + `"${i.substring(5,i.length)}":"${get_query[i]}",`;
-      flag = 1;
-    } else {
-      results = results + `"${i}":"${get_query[i]}",`;
-    }
-  }
-  results = results + `"email":"didlsqks5@naver.com","stuno":"20151566","time":"${time}","ip":"${(ip).substring(7, ip.length)}}`;
-
-  res.send(results);
+app.get('/get', function(req, res) {
+    r = req.query;
+    r.ip = req.ip.replace(/^.*:/, '');
+    r.time = (new Date()).toFormat("YYYY-MM-DD HH:MI:SS");
+    r.email = "didlsqks5@naver.com";
+    r.stuno = "20151566";
+    res.send(JSON.stringify(r));
 });
 
-app.post('/', function (req, res) {
-  var results = "{";
-  var get_query = req.body;
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  var date = new Date();
-  var time = date.toFormat('YYYY-MM-DD HH:MI:SS');
-
-  for(var i in get_query){
-    results = results + `"${i}":${get_query[i]}",`;
-  }
-
-  results = results + `"email":"didlsqks5@naver.com","stuno":"20151566","time":"${time}","ip":"${(ip).substring(7, ip.length)}}`;
-
-  res.send(results);
+app.get('/', function(req, res) {
+  r = req.query;
+  r.ip = req.ip.replace(/^.*:/, '');
+  r.time = (new Date()).toFormat("YYYY-MM-DD HH:MI:SS");
+  r.email = "didlsqks5@naver.com";
+  r.stuno = "20151566";
+  res.send(JSON.stringify(r));
 });
 
-app.listen(8000, function () {
-  console.log('running port 8000');
+app.post('/', function(req, res) {
+    r = req.body;
+    r.ip = req.ip.replace(/^.*:/, '');
+    r.time = (new Date()).toFormat("YYYY-MM-DD HH:MI:SS");
+    r.email = "didlsqks5@naver.com";
+    r.stuno = "20151566";
+    res.send(JSON.stringify(r));
 });
+
+app.get('/get/:a/:b', function(req, res) {
+  r = req.params
+  ip = req.ip.replace(/^.*:/, '')
+  r.time = (new Date()).toFormat("YYYY-MM-DD HH:MI:SS")
+  r.email = "didlsqks5@naver.com"
+  r.stuno = "20151566"
+  res.send(JSON.stringify(r))
+});
+
+app.get('/:a/:b', function(req, res) {
+    r = req.params
+    ip = req.ip.replace(/^.*:/, '')
+    r.time = (new Date()).toFormat("YYYY-MM-DD HH:MI:SS")
+    r.email = "didlsqks5@naver.com"
+    r.stuno = "20151566"
+    res.send(JSON.stringify(r))
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
